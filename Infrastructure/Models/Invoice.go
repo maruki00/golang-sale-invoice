d73037968409ad53cc90e312c99911e68d8572d1
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-02-24 03:39:26 +0300

    --

--- Infrastructure/Models/Invoice.go
+++ Infrastructure/Models/Invoice.go
@@ -1,4 +1,11 @@
 package models
 
+import "time"
+
 type Invoice struct {
+	Id         int       `jaon: id`
+	CustomerId int       `jaon: custumer_id`
+	Total      string    `jaon: total`
+	CreatedAt  time.Time `jaon: created_at`
+	UpdatedAt  time.Time `jaon: updated_at`
 }
