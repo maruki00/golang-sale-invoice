42e55ac51c2c0a18de3d70b862201c8b74745df3
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-02-27 23:55:24 +0300

    --

---
+++ Domain/DTO/CategoryDTO.go
@@ -0,0 +1,12 @@
+package dto
+
+import "time"
+
+type CategoryDTO struct {
+	Id        int       `json:id`
+	Name      string    `json:name`
+	Slug      string    `json:slug`
+	Status    string    `json:status`
+	CreatedAt time.Time `json: created_at`
+	UpdatedAt time.Time `json: updated_at`
+}
