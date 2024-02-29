3351fb609f6f39d60335d47a6856ed3253d6ef0e
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-02-29 05:35:09 +0300

    --

--- Infrastructure/Repositories/MysqlRepository/CategryRepository.go
+++ Infrastructure/Repositories/MysqlRepository/CategryRepository.go
@@ -4,7 +4,15 @@ import (
 	models "saleinvoice/Domain/Entities"
 	"saleinvoice/Infrastructure/database"
 
+	"errors"
+
 	"gorm.io/gorm"
+)
+
+var (
+	errFind   = errors.New("Could Not find Categories")
+	errStore  = errors.New("Could Not Create New Category")
+	errUpdate = errors.New("Could Not Update Category")
 )
 
 type CategoryRepository struct {
@@ -18,32 +26,50 @@ func (cp *CategoryRepository) New() *CategoryRepository {
 	return cp
 }
 
-func (cp CategoryRepository) CreateCategory(category *models.Category) *models.Category {
-	cp.db.Create(category)
-	return category
+func (cp CategoryRepository) CreateCategory(category *models.Category) (*models.Category, error) {
+	err := cp.db.Create(category)
+	if err != nil {
+		return nil, errStore
+	}
+	return category, nil
 }
 
-func (cp CategoryRepository) FindAll() []models.Category {
+func (cp CategoryRepository) FindAll() ([]models.Category, error) {
 	var categories []models.Category
-	cp.db.Find(&categories)
-	return categories
+	err := cp.db.Find(&categories)
+	if err != nil {
+		return nil, errFind
+	}
+	return categories, nil
 }
 
-func (cp CategoryRepository) FindById(id int) *models.Category {
+func (cp CategoryRepository) FindById(id int) (*models.Category, error) {
 	var category models.Category
-	cp.db.Where("Id = ?", id).Find(&category)
-	return &category
+	err := cp.db.Where("Id = ?", id).Find(&category)
+	if err != nil {
+		return nil, errFind
+	}
+	return &category, nil
 }
 
-func (cp CategoryRepository) DeleteCategory(id int) *models.Category {
+func (cp CategoryRepository) DeleteCategory(id int) (*models.Category, error) {
 	var category models.Category
-	cp.db.Where("Id = ?", id).Delete(&category)
-	return &category
+	err := cp.db.Where("Id = ?", id).Delete(&category)
+	if err != nil {
+		return nil, errFind
+	}
+	return &category, nil
 }
 
-func (cp CategoryRepository) UpdateCategory(id int, attributes map[string]interface{}) *models.Category {
+func (cp CategoryRepository) UpdateCategory(id int, attributes map[string]interface{}) (*models.Category, error) {
 	var category models.Category
-	cp.db.Where("Id  = ? ", id).Find(&category)
-	cp.db.Model(&category).Updates(attributes)
-	return &category
+	err := cp.db.Where("Id  = ? ", id).Find(&category)
+	if err != nil {
+		return nil, errFind
+	}
+	err = cp.db.Model(&category).Updates(attributes)
+	if err != nil {
+		return nil, errUpdate
+	}
+	return &category, nil
 }
