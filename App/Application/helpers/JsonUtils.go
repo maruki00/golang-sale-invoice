0767af7eec28cb152ebb33e45ad2c672d92a3c5b
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-03-03 18:14:17 +0300

    --

--- App/Application/helpers/JsonUtils.go
+++ App/Application/helpers/JsonUtils.go
@@ -9,9 +9,9 @@ import (
 	"github.com/mailru/easyjson"
 )
 
-func ParseBody(request *http.Request, x interface{}) {
+func ParseBody(request *http.Request, x &interface{}) {
 	if body, err := io.ReadAll(request.Body); err == nil {
-		if err := easyjson.Unmarshal([]byte(body), &x); err != nil {
+		if err := easyjson.Unmarshal([]byte(body), x); err != nil {
 			log.Fatal(err.Error())
 			fmt.Println("Utils Broken: ", x)
 			return
--- App/Infrastructure/Factories/CategryFactory.go
+++ App/Infrastructure/Factories/CategryFactory.go
@@ -2,14 +2,23 @@ package factories
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
-	"saleinvoice/App/Application/helpers"
+	ej "random/golang/easyJson/model"
 	entities "saleinvoice/App/Domain/Entities"
+
+	"github.com/mailru/easyjson"
 )
 
 func CategoryFactory(request *http.Request) *entities.Category {
-	var category entities.Category
-	helpers.ParseBody(request, &category)
+
+	body, err := io.ReadAll(request.Body)
+	if err != nil {
+		log.Fatal("Error Parse Body")
+	}
+	err = ej.MarshalerUnmarshaler([]byte(body))
+	easyjson.Unmarshal([]byte(body))
 	fmt.Println("factory: ", category)
-	return &category
+	return category
 }
