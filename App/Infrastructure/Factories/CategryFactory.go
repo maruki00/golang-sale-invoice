8abeb3334e21aab40b7cfd41f71c7a9205e6b78c
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-03-05 21:58:13 +0300

    --

--- App/Infrastructure/Factories/CategryFactory.go
+++ App/Infrastructure/Factories/CategryFactory.go
@@ -1,24 +1,17 @@
 package factories
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
-	ej "random/golang/easyJson/model"
 	entities "saleinvoice/App/Domain/Entities"
-
-	"github.com/mailru/easyjson"
 )
 
 func CategoryFactory(request *http.Request) *entities.Category {
-
-	body, err := io.ReadAll(request.Body)
-	if err != nil {
-		log.Fatal("Error Parse Body")
-	}
-	err = ej.MarshalerUnmarshaler([]byte(body))
-	easyjson.Unmarshal([]byte(body))
+	body, _ := io.ReadAll(request.Body)
+	var category *entities.Category
+	json.Unmarshal([]byte(body), category)
 	fmt.Println("factory: ", category)
 	return category
 }
