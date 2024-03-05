66cb51477186690556e1debc7f24181d1d5b8ca8
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-03-05 21:59:14 +0300

    --

--- App/Application/helpers/JsonUtils.go
+++ App/Application/helpers/JsonUtils.go
@@ -1,17 +1,16 @@
 package helpers
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-
-	"github.com/mailru/easyjson"
 )
 
-func ParseBody(request *http.Request, x &interface{}) {
+func ParseBody(request *http.Request, x interface{}) {
 	if body, err := io.ReadAll(request.Body); err == nil {
-		if err := easyjson.Unmarshal([]byte(body), x); err != nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
 			log.Fatal(err.Error())
 			fmt.Println("Utils Broken: ", x)
 			return
